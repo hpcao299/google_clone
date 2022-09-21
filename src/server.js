b6e8698d6570ade3fd818a350e771ecbdef98209
HPhucJ2007 <yourgitemail@example.com>
2022-09-21 21:11:12 +0700

    PORT env

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override');
 const controllers = require('./controllers');
 
 const app = express();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
